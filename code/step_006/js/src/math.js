f15ceeed7f179706964ec9b0b862455441829c2e
rkdud65290 <rkdud6529@gmail.com>
2022-07-01 16:03:55 +0900

    [feat] pakage test

--- code/step_006/js/src/math.js
+++ code/step_006/js/src/math.js
@@ -1,14 +1,21 @@
 //math.js
-const box={};
 
-  (function(){
+
+    const box={};
     const fnSum = (a, b) => a + b;
     const fnMulti = (a, b) => a * b;
+
     function fnDiv(a, b) {
       return a / b;
     };
 
-    box.fnDiv=fnDiv;
-  })()
+    box.div=fnDiv;
+    box.sum=fnSum;
+    box.mul=fnMulti;
+
+    // window.box=box;
+
+    export default box;
+
  
-  console.log('sum:', fnSum(4,3));
+
